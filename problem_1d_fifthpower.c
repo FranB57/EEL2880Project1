7dd60a719679d9121ca7b31def4f8df6e7260885
FranB57 <55671830+FranB57@users.noreply.github.com>
2019-10-03 12:03:22 -0400

    Problem 1d
    finding the 5th power from -11 to 11 using cube and square functions

---
+++ problem_1d_fifthpower.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int square(int y);   //function prototype
+int cube(int y);     //function prototype
+int fifth (int y);   //function prototype
+int main()
+{
+   int x; //counter
+
+    //loop 11 times and calculate and output the fifth power of x each time
+    for ( x=-11; x<=11; ++x){
+        printf("%14d", fifth(x));  //function call
+    } //end for
+    puts("");
+} //end main
+
+// square function definition returns square of its parameter
+int square( int y)
+{
+    return y * y; // returns square of y as int
+}
+
+// cube function definition returns cube of its parameter
+int cube(int y)
+{
+    return y * y *y; // returns cube of y as int
+}
+
+// fifth function definition returns fifth of its parameter
+int fifth(int y)
+{
+    return square(y) * cube(y); // returns fifth power of y as an int
+}
