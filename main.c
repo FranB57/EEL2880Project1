0f2a03ea7d8f8d08c61a71eb093370ff9de11b16
programmerdoge <45884632+programmerdoge@users.noreply.github.com>
2019-10-06 14:11:56 -0400

    Problem 3
    All of Problem 3

--- main.c
+++ main.c
@@ -1,52 +1,57 @@
 #include <stdio.h>
+#define SIZE_1 33
+#define SIZE_2 23
 
-/*
-Problem 1: Printing the factorials of even and odd numbers separately from
-1 to 16
-*/
+int squareFunction(int x); //function prototype for squaring
+int cubeFunction(int y); //function prototype for cubing
 
-unsigned long long int factorial(unsigned int x); //Function Prototype
-
-int main()
+int main(void)
 {
-
-    unsigned int evenCounter; // Variable that will work as a counter for even numbers
-    unsigned int oddCounter; // Variable that will work as a counter for odd numbers
-
-
-    puts("factorials of even numbers: ");
-    //Loop that will start at 0 until 16 and will print the factorial of even numbers
-    for(evenCounter = 0; evenCounter <= 16; evenCounter += 2)
+    
+    //declaration of the arrays that will store the results of raising numbers
+    int squareArray[SIZE_1];
+    int cubeArray[SIZE_1];
+    int fifthPowerArray[SIZE_2];
+    
+    int counter; //variable that will work as counter for the loop
+    
+    printf("%s%29s%37s", "Square of numbers -16 to 16:","Cube of numbers -16 to 16:", "Fifth Power of numbers -11 to 11:\n");
+    
+    //The loop will start in -16 up to 16 to produce and store the values for the square and cube functions
+    for(counter = -16; counter <= 16; ++counter)
     {
-
-        printf("%u! = %llu\n", evenCounter, factorial(evenCounter) );
-
-    }
-
-    puts("\nfactorials of odd numbers: ");
-
-    //Loop that will start at 1 until 15 and will print the factorial of odd numbers
-     for(oddCounter = 1; oddCounter <= 15; oddCounter += 2)
-    {
-
-        printf("%u! = %llu\n", oddCounter, factorial(oddCounter) );
-
-    }
-
-}//End of main
-
-    unsigned long long int factorial(unsigned int x) //Definition of the function that will use recursion
-    {
-
-        if(x <= 1) //Base case that the function will use to guide itself
+        
+        squareArray[counter + 16] = squareFunction(counter);
+        printf("\n%d^2 = %d", counter, squareArray[counter + 16]);
+        
+        cubeArray[counter + 16] = cubeFunction(counter);
+        printf("%23d^3 = %d", counter, cubeArray[counter + 16]);
+        
+        puts("");
+        
+        //the nested if statement will produce and store the values of the fifthPowerArray from -11 to 11
+        if(counter >= -11 && counter <= 11)
         {
-            return 1;
-        }
-         else
-        {
+            
+    //the next statement uses the values already stored in the square and cube arrays to find the new value for fifthPowerArray
+            fifthPowerArray[counter + 11] = squareArray[counter + 16] * cubeArray[counter + 16];
+            printf("%63d^5 = %d", counter, fifthPowerArray[counter + 11]);
+        } // end if statement
+     
+    }// end for loop
+    
+}// end main
 
-            return x * factorial(x - 1);//A function that will call itself to produce recursion
-        }
-    } //End of function definition
+//definition of squareFunction that will multiply a int by itself
+int squareFunction(int x)
+{
+    
+    return x = x * x;
+}
 
-
+//definition of cubeFunction that will muliply 2 times by itself
+int cubeFunction(int y)
+{
+    
+    return y = y * y * y;
+}
